870740e66b35ef7c259638342838ceccefb02b16
ConstantinVigulear <kostavigulear@outlook.com>
2021-05-17 17:54:43 +0300

    Refactor entity more

--- src/main/java/com/stefanini/appointmentapp/AppointmentAppApplication.java
+++ src/main/java/com/stefanini/appointmentapp/AppointmentAppApplication.java
@@ -1,9 +1,11 @@
 package com.stefanini.appointmentapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AppointmentAppApplication {
 
 	public static void main(String[] args) {
