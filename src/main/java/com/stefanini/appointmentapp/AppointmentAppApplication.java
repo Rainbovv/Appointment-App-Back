83061d7e38c03a2eba570a66c8354f53bb997eb1
Turcanu <rturcanu@techteam.com>
2021-05-17 11:10:51 +0300

    Refactor structure

--- src/main/java/com/stefanini/appointmentapp/AppointmentAppApplication.java
+++ src/main/java/com/stefanini/appointmentapp/AppointmentAppApplication.java
@@ -1,4 +1,4 @@
-package stefanini.com.appointmentapp;
+package com.stefanini.appointmentapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
