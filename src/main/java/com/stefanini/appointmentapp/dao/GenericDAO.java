6d3da57c959e8fc2c63407ff28679a0a51c44ea9
Radu Turcanu <turcanu.radu17@gmail.com>
2021-05-25 17:41:24 +0300

    Add docs

--- src/main/java/com/stefanini/appointmentapp/dao/GenericDAO.java
+++ src/main/java/com/stefanini/appointmentapp/dao/GenericDAO.java
@@ -5,17 +5,35 @@ import java.util.List;
 public interface GenericDAO<T> {
 
     /**
-     *
+     * Persists an entity
      * @param t an entity to persist
      * @return persisted t entity
      */
     T create(T t);
 
+    /**
+     * Updates entity
+     * @param t an entity to update
+     * @return updated entity
+     */
     T update(T t);
 
+    /**
+     * Deletes entity
+     * @param t an entity to delete
+     */
     void delete(T t);
 
+    /**
+     * Searches for entity by given id
+     * @param id of an persisted entity
+     * @return found entity
+     */
     T findById(Long id);
 
+    /**
+     * Searches for persisted entities
+     * @return a List of all persisted entities
+     */
     List<T> findAll();
 }
--- src/main/java/com/stefanini/appointmentapp/service/UserService.java
+++ src/main/java/com/stefanini/appointmentapp/service/UserService.java
@@ -2,16 +2,54 @@ package com.stefanini.appointmentapp.service;
 
 import com.stefanini.appointmentapp.dto.RegistrationRequestDto;
 import com.stefanini.appointmentapp.entities.User;
-
 import java.util.List;
 
 public interface UserService {
 
+    /**
+     * Persist an User using UserDaoImpl
+     * @param user to persist
+     * @return persisted user
+     */
     User create(User user);
+
+    /**
+     * Persist an User using UserDaoImpl
+     * @param dto an entity of RegistrationRequestDto
+     * @return persisted user
+     */
     User create(RegistrationRequestDto dto);
+
+    /**
+     * Updates User using UserDaoImpl
+     * @param user to update
+     * @return updated user
+     */
     User update(User user);
+
+    /**
+     * Deletes User using UserDaoImpl
+     * @param user to delete
+     */
     void delete(User user);
+
+    /**
+     * Searches for persisted user by Id using UserDaoImpl
+     * @param id of a User
+     * @return user found ? User : null
+     */
     User findById(Long id);
-    List<User> findAll();
+
+    /**
+     * Searches for persisted user by login using UserDaoImpl
+     * @param login of a User
+     * @return user found ? User : null
+     */
     User findByLogin(String login);
+
+    /**
+     * Search for all persisted users using UserDaoImpl
+     * @return a List of persisted users
+     */
+    List<User> findAll();
 }
