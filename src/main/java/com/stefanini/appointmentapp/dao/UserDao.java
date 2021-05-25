954cc976a2a174ac7e7c54844fdf06e3c181f661
Radu Turcanu <turcanu.radu17@gmail.com>
2021-05-25 17:41:24 +0300

    Add docs

--- src/main/java/com/stefanini/appointmentapp/dao/UserDao.java
+++ src/main/java/com/stefanini/appointmentapp/dao/UserDao.java
@@ -3,5 +3,11 @@ package com.stefanini.appointmentapp.dao;
 import com.stefanini.appointmentapp.entities.User;
 
 public interface UserDao extends GenericDAO<User> {
+
+    /**
+     * Searches for persisted user by login
+     * @param login of an persisted User
+     * @return found user
+     */
     User findByLogin(String login);
 }
--- src/main/java/com/stefanini/appointmentapp/service/AppointmentService.java
+++ src/main/java/com/stefanini/appointmentapp/service/AppointmentService.java
@@ -5,9 +5,36 @@ import java.util.List;
 
 public interface AppointmentService {
 
+    /**
+     * Persists an Appointment using AppointmentDaoImpl
+     * @param appointment to persist
+     * @return persisted appointment
+     */
     Appointment create(Appointment appointment);
+
+    /**
+     * Updates an Appointment using AppointmentDaoImpl
+     * @param appointment to update
+     * @return updated appointment
+     */
     Appointment update(Appointment appointment);
+
+    /**
+     * Deletes an Appointment using AppointmentDaoImpl
+     * @param appointment to delete
+     */
     void delete(Appointment appointment);
+
+    /**
+     * Searches for persisted appointment by Id using AppointmentDaoImpl
+     * @param id of an Appointment
+     * @return user found ? Appointment : null
+     */
     Appointment findById(Long id);
+
+    /**
+     * Searches for all persisted appointment using AppointmentDaoImpl
+     * @return a List of persisted appointments
+     */
     List<Appointment> findAll();
 }
