c7b2f74c2bedc9c8ebf4eaaba5c845258e87f0f3
Radu Turcanu <turcanu.radu17@gmail.com>
2021-05-25 11:05:26 +0300

    Add docs for daos and services

--- src/main/java/com/stefanini/appointmentapp/service/UserService.java
+++ src/main/java/com/stefanini/appointmentapp/service/UserService.java
@@ -5,10 +5,43 @@ import java.util.List;
 
 public interface UserService {
 
+    /**
+     * Persist an User using UserDaoImpl
+     * @param user to persist
+     * @return persisted user
+     */
     User create(User user);
+
+    /**
+     * Updates User using UserDaoImpl
+     * @param user to update
+     * @return updated user
+     */
     User update(User user);
+
+    /**
+     * Deletes User using UserDaoImpl
+     * @param user to delete
+     */
     void delete(User user);
+
+    /**
+     * Searches for persisted user by Id using UserDaoImpl
+     * @param id of a User
+     * @return user found ? User : null
+     */
     User findById(Long id);
+
+    /**
+     * Searches for persisted user by login using UserDaoImpl
+     * @param login of a User
+     * @return user found ? User : null
+     */
     User findByLogin(String login);
+
+    /**
+     * Search for all persisted users using UserDaoImpl
+     * @return a List of persisted users
+     */
     List<User> findAll();
 }
