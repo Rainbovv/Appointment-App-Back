f0bdf8902e8115ccaaf92e60604fbc72237062c2
Radu Turcanu <turcanu.radu17@gmail.com>
2021-06-02 18:25:09 +0300

    Add getAppointmentsByUseId, UserAppointmentDTO

--- src/main/java/com/stefanini/appointmentapp/dao/impl/AppointmentDaoImpl.java
+++ src/main/java/com/stefanini/appointmentapp/dao/impl/AppointmentDaoImpl.java
@@ -1,15 +1,12 @@
 package com.stefanini.appointmentapp.dao.impl;
 
 import com.stefanini.appointmentapp.dao.AppointmentDao;
+import com.stefanini.appointmentapp.dto.UserAppointmentDTO;
 import com.stefanini.appointmentapp.entities.Appointment;
-import com.stefanini.appointmentapp.entities.User;
 import com.stefanini.appointmentapp.entities.UserProfile;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -22,14 +19,27 @@ public class AppointmentDaoImpl extends GenericDAOImpl<Appointment> implements A
     }
 
     @Override
-    public List<Appointment> findByPatientId(Long id) {
+    public List<UserAppointmentDTO> findByUserId(Long id, String userRole) {
+
+        String profileRole = userRole.equals("patient") ? "doctor" : "patient";
 
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Appointment> criteria = builder.createQuery(getEntityClass());
-        Root<Appointment> root = criteria.from(getEntityClass());
-        Join<Appointment, User> join = root.join( "patient" );
-        criteria.select(root);
-        criteria.where(builder.equal(join.get("id"), id));
+        CriteriaQuery<UserAppointmentDTO> criteria = builder
+                .createQuery(UserAppointmentDTO.class);
+
+        Root<Appointment> appointmentRoot = criteria.from(getEntityClass());
+        Root<UserProfile> profileRoot = criteria.from(UserProfile.class);
+
+        criteria.where(builder.equal(appointmentRoot.get(userRole).get("id"), id));
+        criteria.where(builder.equal(profileRoot.get("user").get("id"),
+                appointmentRoot.get(profileRole).get("id")));
+
+        criteria.multiselect(appointmentRoot.get("id"),
+                appointmentRoot.get("startTime"),
+                appointmentRoot.get("endTime"),
+                profileRoot.get("firstName"),
+                profileRoot.get("lastName"),
+                profileRoot.get("office"));
 
         return entityManager.createQuery(criteria).getResultList();
     }
