f3fea588815a63a019da0db2c66ec4b171b40e3b
Victor Svetuskin <victor.svetuskin@extendaretail.com>
2021-06-03 12:23:04 +0300

    Refactor security config

--- src/main/java/com/stefanini/appointmentapp/config/SecurityConfig.java
+++ src/main/java/com/stefanini/appointmentapp/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/api/auth/sign-up").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .and()
-                .csrf().disable().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
--- src/main/java/com/stefanini/appointmentapp/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/stefanini/appointmentapp/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,9 @@
 package com.stefanini.appointmentapp.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -18,6 +21,26 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
         logger.error("Unauthorized user " + e.getMessage());
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String message;
+
+        final Exception exception = (Exception) httpServletRequest.getAttribute("exception");
+
+        if (exception != null) {
+            byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("cause", exception.toString()));
+            httpServletResponse.getOutputStream().write(body);
+        } else {
+            if (e.getCause() != null) {
+                message = e.getCause().toString() + " " + e.getMessage();
+            } else {
+                message = e.getMessage();
+            }
+
+            byte[] body = new ObjectMapper().writeValueAsBytes(Collections.singletonMap("error", message));
+
+            httpServletResponse.getOutputStream().write(body);
+        }
     }
 }
